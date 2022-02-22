05cedd876f6d77b0c22fc9931a924e49ce3082ac
Yun-SeYeong <dbstpdud09@naver.com>
2022-02-22 20:03:28 +0900

    [UPDATE] k값 변경 0.5 -> 0.4

--- Ticker-Collector/src/main/java/demo/coin/controller/CoinHistoryServiceImpl.java
+++ Ticker-Collector/src/main/java/demo/coin/controller/CoinHistoryServiceImpl.java
@@ -167,7 +167,7 @@ public class CoinHistoryServiceImpl implements CoinHistoryService{
                     .candleDateTimeUtc(candleList.get(i).getCandleDateTimeUtc())
                     .targetPrice((candleList.get(i).getHighPrice()
                             .subtract(candleList.get(i).getLowPrice()))
-                            .multiply(new BigDecimal("0.5"))
+                            .multiply(new BigDecimal("0.4"))
                             .add(dayCandleList.get(0).getOpeningPrice()))
                     .build();
 
