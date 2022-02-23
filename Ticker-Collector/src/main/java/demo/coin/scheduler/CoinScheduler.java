9620e93570a3fed34a0761ebf001a917149e1a40
Yun-SeYeong <dbstpdud09@naver.com>
2022-02-23 20:57:19 +0900

    [FIX] 이동평균선 체크 범위 변경 -0.5% -> -1.5%

--- Ticker-Collector/src/main/java/demo/coin/scheduler/CoinScheduler.java
+++ Ticker-Collector/src/main/java/demo/coin/scheduler/CoinScheduler.java
@@ -156,7 +156,7 @@ public class CoinScheduler {
                     checkCurrentBalance();
                 }
 
-                if (isCoinBuy && unit.getBidPrice().compareTo(ma10) < 0 && unit.getBidPrice().compareTo(targetMap.get(ob.getMarket()).getTargetPrice().multiply(BigDecimal.valueOf(0.99))) < 0) {
+                if (isCoinBuy && unit.getBidPrice().compareTo(ma10) < 0 && unit.getBidPrice().compareTo(targetMap.get(ob.getMarket()).getTargetPrice().multiply(BigDecimal.valueOf(0.985))) < 0) {
                     sendSlackHook(SlackMessage.builder()
                             .text("[매도] Coin: " + ob.getMarket() + " Price: " + unit.getBidPrice() + "( 이동평균선 이탈로 인한 손절 [ma10] )")
                             .build());
