9c17491bbb05dfee5936393eb8315e26412fb9e3
Yun-SeYeong <dbstpdud09@naver.com>
2022-04-03 15:21:57 +0900

    [UPDATE] 매수조건 수정

--- Ticker-Collector/src/main/java/demo/coin/scheduler/CoinScheduler.java
+++ Ticker-Collector/src/main/java/demo/coin/scheduler/CoinScheduler.java
@@ -144,7 +144,7 @@ public class CoinScheduler {
                 BigDecimal lastMa60 = lastMaList.get(11);
 
                 if (money >= 10000
-                        && lastMa10.compareTo(lastMa5) > 0
+                        && (lastMa10.compareTo(lastMa5) > 0 || lastMa20.compareTo(lastMa10) > 0 || lastMa60.compareTo(lastMa20) > 0)
                         && ma5.compareTo(ma10) > 0
                         && ma10.compareTo(ma20) > 0
                         && ma20.compareTo(ma60) > 0
